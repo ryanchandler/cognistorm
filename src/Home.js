65a155ef09875157f39dd628f6d8896e0f3f8c6b
ryanchandler <ryan.chandler.35@gmail.com>
2020-10-07 00:30:25 -0500

    update the routing

---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import MicRecorder from 'mic-recorder-to-mp3';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isRecording: false,
+      blobURL: '',
+      isBlocked: false,
+    };
+  }
+
+  
+
+  
+
+  
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+         <h1>Home</h1>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
